177a7eb6b0845dc4b0582b42733993ba3808888f
cristhianlgl <cristhianlgl@hotmail.com>
2024-03-10 22:40:47 -0500

    feat: agregar dashBoardService

--- SistemaVentas.BLL/Servicios/DashhBoardService.cs
+++ SistemaVentas.BLL/Servicios/DashhBoardService.cs
@@ -5,6 +5,7 @@ using SistemaVentas.DTO;
 using SistemaVentas.Model;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,38 @@ namespace SistemaVentas.BLL.Servicios
             _productoRepository = productoRepository;
             _mapper = mapper;
         }
+
+        private IQueryable<Venta> RetornarVentas(IQueryable<Venta> queryVenta, int restarCantDias)
+        {
+            DateTime? ultimaFecha = queryVenta.OrderByDescending(x => x.FechaRegistro)
+                .Select(x => x.FechaRegistro).First();
+            ultimaFecha = ultimaFecha.Value.AddDays(restarCantDias);
+            return queryVenta.Where(x => x.FechaRegistro.Value.Date >= ultimaFecha.Value.Date);
+        }
+
+        private async Task<int> TotalVentasUltimaSemana() 
+        {
+            var queryVenta = await _ventaRepository.Consultar();
+            if(queryVenta.Count() == 0)
+                return 0;
+            var ventas = RetornarVentas(queryVenta, -7);
+            return ventas.Count();  
+        }
+
+        private async Task<string> TotalIngresosUltimaSemana()
+        {
+            var queryVenta = await _ventaRepository.Consultar();
+            if (queryVenta.Count() == 0)
+                return "0";
+            var ventas = RetornarVentas(queryVenta, -7);
+            var result = ventas.Select(x => x.Total).Sum(x => x.Value);
+            return Convert.ToString(result, new CultureInfo("es-CO"));
+        }
+
+        private async Task<int> TotalProductos() =>
+            (await _productoRepository.Consultar()).Count();
+            
+
 
         public Task<DashBoardDTO> ResumenAsync()
         {
