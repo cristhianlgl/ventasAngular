c6d246f40461f7c1889e604feaf696da7c7dab04
Cristhian <cristhianlgl@hotmail.com>
2024-03-14 10:48:29 -0500

    feat(angular): crear componentes y agregar las rutas de la app

--- AppVenta/src/app/app.module.ts
+++ AppVenta/src/app/app.module.ts
@@ -4,14 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { LoginComponent } from './components/login/login.component';
+import { LayoutComponent } from './components/layout/layout.component';
+import { SharedModule } from './reutilizable/shared/shared.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule
   ],
   providers: [
     provideAnimationsAsync()
---
+++ AppVenta/src/app/components/layout/layout-routing.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from './layout.component';
+import { DashBoardComponent } from './pages/dash-board/dash-board.component';
+import { UsuarioComponent } from './pages/usuario/usuario.component';
+import { ProductoComponent } from './pages/producto/producto.component';
+import { ReporteComponent } from './pages/reporte/reporte.component';
+import { VentaComponent } from './pages/venta/venta.component';
+import { HistorialVentaComponent } from './pages/historial-venta/historial-venta.component';
+
+const routes: Routes = [{
+  path:"",
+  component: LayoutComponent,
+  children:[
+    { path:"dashboard", component:DashBoardComponent },
+    { path:"usuario", component:UsuarioComponent },
+    { path:"productos", component:ProductoComponent },
+    { path:"reportes", component:ReporteComponent },
+    { path:"ventas", component:VentaComponent },
+    { path:"historial", component:HistorialVentaComponent },
+  ]
+}];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class LayoutRoutingModule { }
