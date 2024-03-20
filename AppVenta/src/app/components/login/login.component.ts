cc52e0a3aaff3942def0aa8ebe45e9545ed055cc
Cristhian <cristhianlgl@hotmail.com>
2024-03-20 15:03:44 -0500

    feat(angular): agregar clase de utilidad para las alertas y agregar diseño del login

--- AppVenta/src/app/components/login/login.component.ts
+++ AppVenta/src/app/components/login/login.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Login } from '../../interfaces/login';
+import { UsuarioService } from '../../services/usuario.service';
+import { UtilidadService } from '../../reutilizable/utilidad.service';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +11,47 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  
+  formLogin:FormGroup;
+  ocultarClave: boolean = true;
+  mostrarLoading: boolean = false;
+
+  constructor(
+      private formBuilder: FormBuilder, 
+      private router: Router,
+      private _usuarioService: UsuarioService,
+      private _utlidadService: UtilidadService) {
+      this.formLogin =  this.formBuilder.group({
+        email:["", Validators.required],
+        password:["", Validators.required],
+      })
+  }
+
+  iniciarSesion(){
+    this.mostrarLoading =  true;
+    const request:Login =  {
+      correo : this.formLogin.value.email,
+      clave: this.formLogin.value.password
+    }
+    this._usuarioService.iniciarSesion(request).subscribe({
+      next:(data) => {
+        if(data.estatus)
+        {
+          this._utlidadService.guardarSesion(data.valor)
+          this.router.navigate(["pages"]);
+        }
+        else
+        {
+          this._utlidadService.mostrarAlerta("La clave o el usuario no coincide","Opps");
+        }
+      },
+      error:() => { 
+        this._utlidadService.mostrarAlerta("Hubo un error","Opps")
+        this.mostrarLoading = false;
+       },
+      complete:() => { this.mostrarLoading = false }
+    })
+  }
+  
 
 }
--- AppVenta/src/app/reutilizable/utilidad.service.ts
+++ AppVenta/src/app/reutilizable/utilidad.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Sesion } from '../interfaces/sesion';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UtilidadService {
 
-  constructor() { }
+  constructor(private matSnackBar : MatSnackBar) { }
+
+  mostrarAlerta(mensaje:string, tipo:string){
+    this.matSnackBar.open(mensaje, tipo, { 
+      horizontalPosition: "end",
+      verticalPosition: "top",
+      duration: 3000
+    })
+  }
+
+  guardarSesion(sesion:Sesion){
+    localStorage.setItem("sesion",JSON.stringify(sesion))
+  }
+
+  obtenerUsuario(){
+    const data = localStorage.getItem("sesion");
+    return JSON.parse(data!);
+  }
+
+  eliminarUsuario(){
+    localStorage.removeItem("sesion");
+  }
 }
